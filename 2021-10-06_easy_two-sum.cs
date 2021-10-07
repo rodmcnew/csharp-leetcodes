ddffde1beec03d79d128fb38eff7f4c87c5c0d25
Rod McNew <rmcnew@relivinc.com>
2021-10-06 20:20:22 -0500

    Create 2021-10-06_easy_two-sum.cs

---
+++ 2021-10-06_easy_two-sum.cs
@@ -0,0 +1,57 @@
+// Compared to the  solution further below, this one uses about 500KB less memory but may be harder to understand. It is the same speed.
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        // Create an array of the num indicies sorted by num value.
+        // This gives us a sorted view of the nums while retaining their original indicies.
+        var sortedIndicies = Enumerable.Range(0, nums.Length).OrderBy(i => nums[i]).ToArray();
+        
+        // Start with a pointer on each side of the sorted nums       
+        // Then move the left and right pointers inward until we find the match
+        int left = 0;
+        int right = sortedIndicies.Length - 1;
+        while(left < right) {
+            int leftI = sortedIndicies[left];
+            int rightI = sortedIndicies[right];
+            int sum = nums[leftI] + nums[rightI];
+            if(sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[] {leftI, rightI};
+            }
+        }
+        throw new ArgumentException("No solution found");
+    }
+}
+
+
+
+
+//////// Another older solution below ////////
+
+
+
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        // Get the nums into a data structure that we can sort while remembering their indicies
+        var sortedNums = nums.Select((Value, Index) => new { Value, Index }).OrderBy(num => num.Value).ToArray();
+        
+        // Start with a pointer on each side of the sorted nums       
+        // Then move the left and right pointers inward until we find the match
+        int left = 0;
+        int right = sortedNums.Length - 1;
+        while(left < right) {
+            int sum = sortedNums[left].Value + sortedNums[right].Value;
+            if(sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[] {sortedNums[left].Index, sortedNums[right].Index};
+            }
+        }
+        throw new ArgumentException("No solution found");
+    }
+}
